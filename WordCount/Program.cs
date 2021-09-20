fdf3e8956132e12f87bd6dab89f945054c4a9ac2
--replace-all <brodersen88@gmail.com>
2021-09-20 07:30:59 +0200

    Changes

--- WordCount/Program.cs
+++ WordCount/Program.cs
@@ -13,109 +13,43 @@ namespace WordCount
             //Setting variables
             var watchSearch = new System.Diagnostics.Stopwatch();
             var watchDisplay = new System.Diagnostics.Stopwatch();
+            List<string> textSplit = new();
             Dictionary<string, int> dic = new();
 
-            int numberOfGroups = Environment.ProcessorCount;
-            int finished = 0;
+            Console.WriteLine("File path:");
+            string path = Console.ReadLine();
 
             //Start indexing
             watchSearch.Start();
-            
-            string text = File.ReadAllText("../../../file.txt").ToLower();
-            string[] textSplit = text.Split(new[] { Environment.NewLine, " ", ",",".","--", "\"" }, StringSplitOptions.RemoveEmptyEntries);
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    textSplit.AddRange(line.Split(new[] { Environment.NewLine, " ", ",", ".", "\"" }, StringSplitOptions.RemoveEmptyEntries));
+                }
+            }
 
             //Counting
             foreach(string s in textSplit)
             {
-                if (dic.ContainsKey(s))
-                    dic[s]++;
-                else
+                if (!dic.ContainsKey(s))
                     dic.Add(s, 1);
+                else
+                    dic[s]++;
             }
-            int uord = dic.Count;
-
             watchSearch.Stop();
 
             //Start Displaying
             watchDisplay.Start();
+            List<string> outList = dic.Select((x) => $"{x}").ToList();
+            File.WriteAllLinesAsync("WriteLines.txt", outList);
+            watchDisplay.Stop();
 
-            Action onCompleted = () =>
-            {
-                if (finished == numberOfGroups - 1)
-                {
-                    Console.WriteLine();
-                    Console.WriteLine($"Unikke ord: {uord}");
-                    watchDisplay.Stop();
-                    Console.WriteLine($"Hyppiste ord: {dic.OrderByDescending(x => x.Value).First().ToString()}");
-                    Console.WriteLine($"Total time: {watchDisplay.ElapsedMilliseconds + watchSearch.ElapsedMilliseconds}ms, Search time: {watchSearch.ElapsedMilliseconds}ms, Display time: {watchDisplay.ElapsedMilliseconds}ms");
-                    Console.ReadLine();
-                }
-                else
-                    finished++;
-            };
-
-            //Converts dictionary to list of lists of keyvalue pairs
-            var dicList = dic
-                        .Select((x, i) => new { Index = i, Value = x })
-                        .GroupBy(x => x.Index / ((uord / numberOfGroups)))
-                        .Select(x => x.Select(v => v.Value).ToList())
-                        .ToList();
-
-            for (int i = 0; i < dicList.Count -1 ; i++)
-            {
-                Thread thread = new Thread(() =>
-                {
-                    try
-                    {
-                        print(dicList[i]);
-                    }
-                    finally
-                    {
-                        onCompleted();
-                    }
-                });
-                thread.Start();
-            }
-        }
-
-        private static void print(List<KeyValuePair<string, int>> list)
-        {
-            //Pairs in row
-            int r = 10;
-            //Colum width
-            int space = 32;
-            for (int i = 0; i < list.Count; i+= r)
-            {
-                string s = "";
-                for (int j = 0; j < r; j++)
-                {
-                    try
-                    {
-                        string st = $"[{list[i + j].Value}] {list[i + j].Key}";
-                        s += st;
-                        //Spaceing
-                        for (int k = 0; k < space - st.Length; k++)
-                        {
-                            s += " ";
-                        }
-                    }
-                    catch
-                    {
-                        //stop if no more elements in list
-                    }
-                }
-                Console.WriteLine(s);
-            }
-        }
-    }
-
-    public static class Extensions
-    {
-        public static T[] SubArray<T>(this T[] array, int offset, int length)
-        {
-            T[] result = new T[length];
-            Array.Copy(array, offset, result, 0, length);
-            return result;
+            Console.WriteLine($"Hyppiste ord: {dic.OrderByDescending(x => x.Value).First()}");
+            Console.WriteLine($"Total time: {watchDisplay.ElapsedMilliseconds + watchSearch.ElapsedMilliseconds}ms, Search time: {watchSearch.ElapsedMilliseconds}ms, Display time: {watchDisplay.ElapsedMilliseconds}ms");
+            Console.ReadLine();
         }
     }
 }
